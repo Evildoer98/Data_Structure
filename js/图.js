4a87473f69dfeed767c4c475cfd474bc7649aabc
Evildoer98 <1213089093@qq.com>
2021-05-04 23:09:52 +0800

    v6

--- js/图.js
+++ js/图.js
@@ -55,4 +55,59 @@ function Graph () {
         return resultStr
     }
 
+    // 初始化颜色代码
+    Graph.prototype.initializeColor = function () {
+        var color = []
+        for (let i = 0; i < this.vertexes.length; i++) {
+            colors[this.vertexes[i]] = "white"
+        }
+        return colors
+    }
+
+    // 广度优先搜索算法
+    /**
+     * 
+     * 广度优先算法会先从指定的第一个顶点开始遍历图，先访问其所有的相邻点，就像一次访问图的一层
+     * 就是先宽后深的访问顶点
+     * 
+     * 实现过程：
+     *  创建一个队列 Q
+     *  将 v 标注为被发现的（灰色），并将 v 放入 队列 Q
+     *  如果队列 Q 非空，则
+     *      将 v 从 Q 中取出队列
+     *      将 v 标注为被发现的灰色
+     *      将 v 所有的未被访问过的邻接点（白色），加入到队列中
+     *      将 v 标志位黑色
+     */
+    Graph.prototype.bfs = function (v, handler) {
+        // 初始化颜色
+        var color = this.initializeColor()
+        // 创建队列
+        var queue = new Queue()
+        // 将传入的顶点放入队列中
+        while (!queue.isEmpty()) {
+            // 从队列中取出数据
+            var qv = queue.dequeue()
+            // 获取 qv 相邻的所有顶点
+            var qAdj = this.adjList.get(qv)
+            // 将颜色设置为灰色
+            color[qv] = "gray"
+            // 将 qAdj 的所有顶点依次压入队列中
+            for (let i = 0; i < qAdj.length; i++) {
+                var a = qAdj[i]
+                if (color[a] === "white") {
+                    color[a] = "gray"
+                    queue.enqueue(a)
+                }
+            }
+            // 因为 qv 已经探测完毕，将 qv 设置成黑色
+            color[qv] = "black"
+            // 处理 qv
+            if (handler) {
+                handler(qv)
+            }
+        }
+    }
+
+    
 }
